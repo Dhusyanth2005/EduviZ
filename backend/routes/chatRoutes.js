942bf56d2762d6e95f94cc932f60ebca8f95560c
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-03-26 20:17:17 +0530

    Updated backed

--- backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { preloadData, chat } = require('../controllers/chatController');
-const router = express.Router();
-
-router.post('/preload', preloadData);
-router.post('/chat', chat);
-
-module.exports = router;
